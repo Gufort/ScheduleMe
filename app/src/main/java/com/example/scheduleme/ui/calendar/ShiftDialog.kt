16d7b8bd95011f2a2db0ec0b4a52929684474f62
Gufort <guffy911d@gmail.com>
2026-08-12 00:11:31 +0000

    Add ShiftDialog template

--- app/src/main/java/com/example/scheduleme/ui/calendar/ShiftDialog.kt
+++ app/src/main/java/com/example/scheduleme/ui/calendar/ShiftDialog.kt
@@ -1,5 +1,6 @@
 package com.example.scheduleme.ui.calendar
 
+import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
@@ -11,20 +12,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import java.time.LocalDate
 import java.time.LocalTime
 
+@SuppressLint("DefaultLocale")
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ShiftDialog(
@@ -33,6 +38,10 @@ fun ShiftDialog(
 ){
     var name by remember { mutableStateOf("") }
     var startTime by remember { mutableStateOf(LocalTime.of(9, 0)) }
+    var endTime by remember { mutableStateOf(LocalTime.of(9, 0)) }
+    var showEndTimePicker by remember { mutableStateOf(false) }
+    var showStartTimePicker by remember { mutableStateOf(false) }
+    var description by remember { mutableStateOf("") }
     Dialog(
         onDismissRequest = onDismiss
     ) {
@@ -63,7 +72,7 @@ fun ShiftDialog(
                     value = name,
                     onValueChange = { name = it },
                     label = { Text("Название") },
-                    placeholder = { Text("Ввод...") },
+                    placeholder = { Text("Введите текст...") },
                     modifier = Modifier.fillMaxWidth(),
                     singleLine = true
                 )
@@ -74,7 +83,94 @@ fun ShiftDialog(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
+                    OutlinedButton(
+                        onClick = { showStartTimePicker = true },
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text("Начало")
+                            Text(
+                                text = String.format(
+                                    "%02d:%02d",
+                                    startTime.hour,
+                                    startTime.second
+                                )
+                            )
+                        }
+                    }
 
+                    OutlinedButton(
+                        onClick = {
+                            showEndTimePicker = true
+                        },
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text("Конец")
+                            Text(
+                                text = String.format(
+                                    "%02d:%02d",
+                                    endTime.hour,
+                                    endTime.minute
+                                )
+                            )
+                        }
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    label = { Text(text = "Описание") },
+                    placeholder = { Text(text = "Введите текст...") },
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Absolute.SpaceBetween
+                ){
+                    TextButton(onClick = onDismiss) {
+                        Text(text = "Создать")
+                    }
+                    TextButton(onClick = onDismiss) {
+                        Text(text = "Отмена")
+                    }
+                }
+
+                if (showStartTimePicker) {
+                    TimePickerDialog(
+                        initialTime = startTime,
+                        onTimeSelected = { time ->
+                            startTime = time
+                            showStartTimePicker = false
+                        },
+                        onDismiss = {
+                            showStartTimePicker = false
+                        }
+                    )
+                }
+
+                if (showEndTimePicker) {
+                    TimePickerDialog(
+                        initialTime = endTime,
+                        onTimeSelected = { time ->
+                            endTime = time
+                            showEndTimePicker = false
+                        },
+                        onDismiss = {
+                            showEndTimePicker = false
+                        }
+                    )
                 }
             }
         }
